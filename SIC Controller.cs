54d21552e76818f7883e84aa9db7db3668f2bf59
citizenslave <44645653+citizenslave@users.noreply.github.com>
2019-06-02 21:32:36 -0400

    Update SIC Controller.cs
    Inventory sorting.

--- SIC Controller.cs
+++ SIC Controller.cs
@@ -17,11 +17,71 @@ public void Save() {
 public void Main(string argument, UpdateType updateSource) {
     if ((updateSource & UpdateType.Update100) != 0) {
         updateSource &= ~UpdateType.Update100;
+        SortInventory();
         DisplayInventory();
     }
     if (updateSource != UpdateType.None) {
         // execute commands
     }
+}
+
+void SortInventory(Boolean connected = false) {
+    List<IMyCargoContainer> storageContainers = new List<IMyCargoContainer>();
+    GridTerminalSystem.GetBlocksOfType(storageContainers, IsConnected);
+
+    List<IMyTerminalBlock> containers = new List<IMyTerminalBlock>();
+    GridTerminalSystem.GetBlocksOfType(containers, block => {
+        if (!IsConnected(block) && !connected) return false;
+        if (!block.HasInventory) return false;
+
+        if (block.BlockDefinition.ToString().Contains("Reactor")) return false;
+        if (block.BlockDefinition.ToString().Contains("OxygenGenerator")) return false;
+        if (block.BlockDefinition.ToString().Contains("OxygenTank")) return false;
+
+        if (block.BlockDefinition.ToString().Contains("Refinery")) {
+            SortBlockInventory(block, block.GetInventory(1), storageContainers);
+            return true;
+        } else if (block.BlockDefinition.ToString().Contains("Assembler")) {
+            if ((block as IMyAssembler).IsQueueEmpty) SortBlockInventory(block, block.GetInventory(0), storageContainers);
+            SortBlockInventory(block, block.GetInventory(1), storageContainers);
+            return true;
+        } else if (block.BlockDefinition.ToString().Contains("Container") ||
+                block.BlockDefinition.ToString().Contains("Connector")) {
+            SortBlockInventory(block, block.GetInventory(0), storageContainers);
+            return true;
+        }
+
+        else Echo($"{ block.BlockDefinition.ToString() } block inventories not sorted.");
+
+        return true;
+    });
+}
+
+void SortBlockInventory(IMyTerminalBlock block, IMyInventory inventory,
+        List<IMyCargoContainer> storageContainers) {
+    List<MyInventoryItem> items = new List<MyInventoryItem>();
+    inventory.GetItems(items);
+    foreach (MyInventoryItem item in items) {
+        Boolean transferred = false;
+        Boolean overflow = false;
+        foreach (IMyCargoContainer container in storageContainers.OrderBy(i => GetPriority(i.CustomData, item.Type))) {
+            IMyInventory destinationInventory = container.GetInventory(0);
+            transferred = inventory.TransferItemTo(destinationInventory, item);
+            overflow = GetPriority(container.CustomData, item.Type) > storageContainers.Count;
+            if (transferred || inventory.Equals(destinationInventory)) break;
+        }
+        if ((overflow || !transferred) && !storageContainers.Contains(block))
+            Echo($"Insufficient Storage for { FormatItemType(item.Type) } ({ item.Amount }) - { block.CustomName }");
+    }
+}
+
+int GetPriority(string customData, MyItemType type) {
+    string priorityType = type.ToString().Contains("Ice")?"iceStoragePriority":
+            type.ToString().Contains("Ammo")?"ammoStoragePriority":
+            $"{ FormatItemType(type).Split('/')[0].ToLower() }StoragePriority";
+    string[] dataItems = customData.Split('\n');
+    foreach (string dataItem in dataItems) if (dataItem.Contains(priorityType)) return int.Parse(dataItem.Split('=')[1]);
+    return int.MaxValue;
 }
 
 void DisplayInventory(Boolean connected = false) {
@@ -51,7 +111,8 @@ void DisplayInventory(Boolean connected = false) {
 }
 
 string FormatItemType(MyItemType type) {
-    return type.ToString().Split('_')[1];
+    string typeString = type.ToString();
+    return typeString.Substring(typeString.IndexOf("_")+1);
 }
 
 string FormatItemAmount(MyFixedPoint amount) {
