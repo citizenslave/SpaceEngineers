d22a9bbd966695ee549e1a3dcd0357bebaf45a0c
citizenslave <44645653+citizenslave@users.noreply.github.com>
2019-06-08 21:21:58 -0400

    Update SIC Controller.cs
    Add refinery prioritization

--- SIC Controller.cs
+++ SIC Controller.cs
@@ -3,9 +3,13 @@ List<IMyTextSurface> cargoDisplays = new List<IMyTextSurface>();
 List<IMyTextSurface> rsnDisplays = new List<IMyTextSurface>();
 IMyRadioAntenna antenna;
 string RSN_CHANNEL = "RSN";
-
 Dictionary<long,string> rsnStatusData = new Dictionary<long,string>();
 Dictionary<long,int> rsnAgeData = new Dictionary<long,int>();
+
+List<IMyCargoContainer> storageContainers = new List<IMyCargoContainer>();
+
+Dictionary<IMyRefinery,Dictionary<string,int>> RefineryPriorities = new Dictionary<IMyRefinery,Dictionary<string,int>>();
+Dictionary<IMyRefinery,List<IMyTextSurface>> RefineryDisplays = new Dictionary<IMyRefinery,List<IMyTextSurface>>();
 
 public Program() {
     Runtime.UpdateFrequency = UpdateFrequency.Update100;
@@ -37,10 +41,94 @@ public void Main(string argument, UpdateType updateSource) {
 
         FindRSNDisplays();
         UpdateRSNDisplays();
+
+        PrioritizeRefining();
     }
     if (updateSource != UpdateType.None) {
         if (argument.Equals(RSN_CHANNEL)) FetchPacket(RSN_CHANNEL);
+        else if (argument.Split()[0].ToUpper().Contains("REFINERY")) ProcessRefineryCommands(argument);
     }
+}
+
+void ProcessRefineryCommands(string command) {
+    MyCommandLine refineryCommand = new MyCommandLine();
+    if (refineryCommand.TryParse(command)) {
+        IMyRefinery refinery = GridTerminalSystem.GetBlockWithName(refineryCommand.Argument(2)) as IMyRefinery;
+        if (refinery != null) {
+            if (refineryCommand.Argument(1).ToUpper().Equals("CLEAR")) {
+                SortBlockInventory(refinery, refinery.GetInventory(0), storageContainers);
+                Echo($"{ refinery.CustomName } input inventory cleared...");
+            } else Echo($"Unknown Refinery Command:\n\"{ refineryCommand.Argument(1) }\"");
+        } else Echo($"Cannot find refinery with -name \"{ refineryCommand.Argument(2) }\"");
+    }
+}
+void PrioritizeRefining() {
+    List<IMyRefinery> refineries = new List<IMyRefinery>();
+    GridTerminalSystem.GetBlocksOfType(refineries, block => {
+        if (!IsConnected(block)) return false;
+        IMyRefinery refinery = (IMyRefinery) block;
+        MyIni refineryConfig = new MyIni();
+        MyIniParseResult result = new MyIniParseResult();
+        if (refineryConfig.TryParse(block.CustomData, out result)) {
+            if (refineryConfig.ContainsSection("Priority")) {
+                RefineryPriorities[refinery] = new Dictionary<string,int>();
+                RefineryDisplays[refinery] = new List<IMyTextSurface>();
+                GridTerminalSystem.GetBlocksOfType(RefineryDisplays[refinery], surface => {
+                    IMyTextPanel panel = (IMyTextPanel) surface;
+                    if (!panel.CustomData.Contains($"[LCD]\nRefineryDisplay=\"{ refinery.CustomName }\"")) return false;
+                    surface.ContentType = ContentType.TEXT_AND_IMAGE;
+                    surface.FontColor = Color.DarkSlateGray;
+                    return true;
+                });
+                Display(RefineryDisplays[refinery], $"{ refinery.CustomName }\nPRIORITIES\n", false);
+                List<MyIniKey> priorityKeys = new List<MyIniKey>();
+                refineryConfig.GetKeys("Priority", priorityKeys);
+                foreach(MyIniKey priorityKey in priorityKeys.OrderBy(i => refineryConfig.Get(i).ToInt32(int.MaxValue))) {
+                    string oreKey = priorityKey.ToString().Split('/')[1];
+                    RefineryPriorities[refinery][oreKey] = refineryConfig.Get("Priority", oreKey).ToInt32(int.MaxValue);
+                    Display(RefineryDisplays[refinery], $"{ oreKey }: { SortOre(oreKey, refinery) }\n");
+                }
+            }
+        }
+        return true;
+    });
+}
+
+string SortOre(string oreKey, IMyRefinery refinery) {
+    MyFixedPoint amount = 0;
+    IMyInventory refineryInventory = refinery.GetInventory(0);
+    MyItemType oreToFind = new MyItemType("MyObjectBuilder_Ore", oreKey);
+    List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+    GridTerminalSystem.GetBlocksOfType(blocks, block => {
+        if (!block.HasInventory) return false;
+        IMyInventory inventory = block.GetInventory(0);
+        int itemSlot = FindItemSlot(inventory, oreToFind);
+        if (itemSlot != -1) amount += ((MyInventoryItem) inventory.GetItemAt(itemSlot)).Amount;
+        else return false;
+        if (!IsConnected(block)) {
+            int toSlot = FindItemSlot(refineryInventory, oreToFind);
+            refineryInventory.TransferItemFrom(inventory, itemSlot, toSlot>=0?toSlot:refineryInventory.ItemCount, true);
+        }
+        while (itemSlot != -1) {
+            itemSlot = FindItemSlot(inventory, oreToFind, itemSlot);
+            if (itemSlot != -1) amount += ((MyInventoryItem) inventory.GetItemAt(itemSlot)).Amount;
+        }
+        return !RefineryPriorities.Keys.Contains(block) && !IsConnected(block);
+    });
+    foreach (IMyTerminalBlock block in blocks) {
+        IMyInventory foundInventoryOnConstruct = block.GetInventory(0);
+        int toSlot = FindItemSlot(refineryInventory, oreToFind);
+        refineryInventory.TransferItemFrom(foundInventoryOnConstruct,
+                FindItemSlot(foundInventoryOnConstruct, oreToFind), toSlot>=0?toSlot:refineryInventory.ItemCount, true);
+    }
+    return $"{ ((float) amount).ToString("n2") }kg";
+}
+
+int FindItemSlot(IMyInventory inventory, MyItemType itemType, int startIndex = -1) {
+    for (int i=startIndex+1; i<inventory.ItemCount; i++) {
+        if (((MyInventoryItem) inventory.GetItemAt(i)).Type.Equals(itemType)) return i;
+    }
+    return -1;
 }
 
 void FetchPacket(string channel) {
@@ -78,7 +166,7 @@ void UpdateRSNDisplays() {
 }
 
 void SortInventory(Boolean connected = false) {
-    List<IMyCargoContainer> storageContainers = new List<IMyCargoContainer>();
+    storageContainers.Clear();
     GridTerminalSystem.GetBlocksOfType(storageContainers, IsConnected);
 
     List<IMyTerminalBlock> containers = new List<IMyTerminalBlock>();
